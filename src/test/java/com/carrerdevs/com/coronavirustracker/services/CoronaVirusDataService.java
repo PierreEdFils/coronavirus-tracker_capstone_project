145afed3eda73d1c0de807c61605a10023247ba8
Pierre Fils <epierrefils@gmail.com>
2023-01-21 12:24:37 -0500

    coronavirus-tracker updated

--- src/test/java/com/carrerdevs/com/coronavirustracker/services/CoronaVirusDataService.java
+++ src/test/java/com/carrerdevs/com/coronavirustracker/services/CoronaVirusDataService.java
@@ -23,6 +23,11 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL ="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData () throws IOException, InterruptedException {
@@ -40,8 +45,7 @@ public class CoronaVirusDataService {
             locationStats.setState(record.get("Province/State"));
             locationStats.setCountry(record.get("Country/Region"));
             locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
-            System.out.println(locationStats);
-            newStats.add(locationStats);
+
         }
         this.allStats=newStats;
     }
